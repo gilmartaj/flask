b401f1cbbf0a223bca0e78b567dca0ef2be24ae2
gilmartaj <44650363+gilmartaj@users.noreply.github.com>
2023-06-27 09:36:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route("/asaas", methods=['POST'])
+def index():
+    print(request.json)
+    return jsonify(success=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
