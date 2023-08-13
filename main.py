7038a6bde554337241b49d03c7279b63b02dc1c1
gilmartaj <gilmartaj@gmail.com>
2023-08-13 11:45:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ def wh():
     return jsonify(success=True)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+#if __name__ == '__main__':
+#    app.run(debug=True, port=os.getenv("PORT", default=5000))
